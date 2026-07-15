cb0cea204c72b3cf74c795b78928d1e90b5562ba
Jitu1209 <110666211+Jitu1209@users.noreply.github.com>
2026-07-15 22:37:24 +0530

    fix: resolve merge conflicts and build production with new grid background logic

---
+++ dist/assets/check-BYFQ9rGT.js
@@ -0,0 +1,1 @@
+import{v as e}from"./index-ta4Y7ujW.js";var t=e(`check`,[[`path`,{d:`M20 6 9 17l-5-5`,key:`1gmf2c`}]]);export{t};
---
+++ dist/assets/chevron-up-VPxqBBVk.js
@@ -0,0 +1,1 @@
+import{v as e}from"./index-ta4Y7ujW.js";var t=e(`chevron-up`,[[`path`,{d:`m18 15-6-6-6 6`,key:`153udz`}]]);export{t};
