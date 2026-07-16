d76f7f0359bd7511f47ec83cb9678eb8471b0bbc
Vishal Singh Chhonkar <vishal@Vishals-MacBook-Air.local>
2026-07-16 19:27:23 +0530

    build: compile assets with real Google Client ID

---
+++ dist/assets/check-DZ9tGhVu.js
@@ -0,0 +1,1 @@
+import{v as e}from"./index-RAxc_WsA.js";var t=e(`check`,[[`path`,{d:`M20 6 9 17l-5-5`,key:`1gmf2c`}]]);export{t};
---
+++ dist/assets/chevron-up-Drx-WCck.js
@@ -0,0 +1,1 @@
+import{v as e}from"./index-RAxc_WsA.js";var t=e(`chevron-up`,[[`path`,{d:`m18 15-6-6-6 6`,key:`153udz`}]]);export{t};
